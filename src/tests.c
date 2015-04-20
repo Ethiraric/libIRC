9df5c529e4dbef0a5b307a1fd4ba626da65592c1
sabour_f <sabour_f@epitech.eu>
2015-04-20 10:58:04 +0200

    Forgot test file

---
+++ src/tests.c
@@ -0,0 +1,92 @@
+/*
+** tests.c for  in /home/sabour_f/rendu/libIRC
+**
+** Made by Florian SABOURIN
+** Login   <sabour_f@epitech.net>
+**
+** Started on  Wed Apr 15 00:48:34 2015 Florian SABOURIN
+** Last update Wed Apr 15 01:05:46 2015 Florian SABOURIN
+*/
+
+#include <assert.h>
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "t_string.h"
+
+void		test_t_string()
+{
+  t_string	a, b;
+
+
+  // Act
+  str_new(&a);
+
+  // Assert
+  assert(!str_str(&a));
+  assert(str_safestr(&a));
+  assert(!strcmp(str_safestr(&a), ""));
+
+  /* ----------------------------- */
+
+  // Act
+  if (str_newfromcstr(&a, "coucou"))
+    err(1, NULL);
+
+  // Assert
+  assert(str_str(&a));
+  assert(str_safestr(&a));
+  assert(!strcmp(str_str(&a), "coucou"));
+  assert(str_size(&a) == 6);
+
+  /* ----------------------------- */
+
+  // Act
+  if (str_newfromstr(&b, &a))
+    err(1, NULL);
+
+  assert(str_str(&a));
+  assert(str_safestr(&a));
+  assert(!strcmp(str_str(&a), "coucou"));
+  assert(str_size(&a) == 6);
+  assert(str_str(&b));
+  assert(str_safestr(&b));
+  assert(!strcmp(str_str(&b), "coucou"));
+  assert(str_size(&b) == 6);
+  assert(str_str(&a) == str_str(&b));
+
+  /* ----------------------------- */
+
+  // Act
+  if (str_detach(&a))
+    err(1, NULL);
+
+  // Assert
+  assert(str_str(&a));
+  assert(str_safestr(&a));
+  assert(!strcmp(str_str(&a), "coucou"));
+  assert(str_size(&a) == 6);
+  assert(str_str(&b));
+  assert(str_safestr(&b));
+  assert(!strcmp(str_str(&b), "coucou"));
+  assert(str_size(&b) == 6);
+  assert(str_str(&a) != str_str(&b));
+
+  /* ----------------------------- */
+
+  // Act
+  str_delete(&a);
+
+  // Assert
+  assert(!str_str(&a));
+  assert(str_safestr(&a));
+  assert(!strcmp(str_safestr(&a), ""));
+}
+
+int		main()
+{
+  test_t_string();
+  printf("All the tests passed\n");
+  return (0);
+}
