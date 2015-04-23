75c25d9877674220e5d929cf2d9d2a8607a13d72
sabour_f <sabour_f@epitech.eu>
2015-04-23 17:01:55 +0200

    Forgot to add the file

---
+++ src/irc_cmd_ping.c
@@ -0,0 +1,19 @@
+/*
+** irc_cmd_ping.c for  in /home/sabour_f/rendu/libIRC/src
+**
+** Made by Florian SABOURIN
+** Login   <sabour_f@epitech.net>
+**
+** Started on  Thu Apr 23 16:26:18 2015 Florian SABOURIN
+** Last update Thu Apr 23 16:26:18 2015 Florian SABOURIN
+*/
+
+#include "irc.h"
+
+int		cmd_ping(t_ircconnection *irc)
+{
+  if (!irc->cmd.argc)
+    return (irc_send_formatted(irc, "PONG\r\n"));
+  return (irc_send_formatted(irc, "PONG :%s\r\n",
+			     irc->cmd.args[irc->cmd.argc - 1]));
+}
